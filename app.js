f54e9cd1433c188e452356a1e962360407419831
Antonio <antoniozamora21331@gmail.com>
2023-09-04 19:30:58 -0700

    Fix a small bug and add a win screen

--- app.js
+++ app.js
@@ -5,6 +5,23 @@ const Player = (name, sign) => {
 const player1 = Player('Player 1', 'X');
 const player2 = Player('Player 2', 'O');
 
+const showWinner = document.getElementById('show-winner');
+const overlay = document.querySelector('.overlay');
+let win = document.querySelector('.win');
+
+const winForm = (currentPlayer) => {
+    showWinner.classList.add('active');
+    overlay.classList.add('active');
+    win.innerText = currentPlayer + " Wins!";
+}
+
+const winFormClose = () => {
+    showWinner.classList.remove('active');
+    overlay.classList.remove('active');
+}
+
+overlay.onclick = winFormClose;
+
 let indicator = document.querySelector('.indicator');
 
 const gameBoard = (() => {
@@ -37,15 +54,12 @@ const gameBoard = (() => {
     Array.from(spaces.children).forEach((space, i) => {
         space.addEventListener('click', () => {
             if (game.won == false) {
-                if (space.innerText === '') {
-                    space.innerText += game.currentPlayer.sign;
-                }
                 space.setAttribute('data', game.currentPlayer.sign);
                 board[i] = game.currentPlayer.sign;
                 game.checkWinner();
-                game.spacesLeft -= 1;
-                game.currentPlayer === player1 ? space.style.backgroundColor = '#1D3461' : space.style.backgroundColor = '#FFB8D1';
-                if (game.won === false) {
+                if (space.innerText === '') {
+                    space.innerText += game.currentPlayer.sign;
+                    game.currentPlayer === player1 ? space.style.backgroundColor = '#1D3461' : space.style.backgroundColor = '#FFB8D1';
                     if (game.currentPlayer === player1) {
                         game.currentPlayer = player2;
                         indicator.innerText = "Player 2's Turn";
@@ -81,8 +95,8 @@ const game = (() => {
         winning.forEach((item, i) => {
             if (gameBoard.board[item[0]] === this.currentPlayer.sign && gameBoard.board[item[1]] === this.currentPlayer.sign && gameBoard.board[item[2]] === this.currentPlayer.sign) {
                 console.log(this.currentPlayer.name + ' Wins!');
+                winForm(this.currentPlayer.name);
                 this.won = true;
-                indicator.innerText = this.currentPlayer.name + " Wins!";
             }
         })
     }
